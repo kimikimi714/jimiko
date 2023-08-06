44b29ea7effbc25fa10ecc6d4a7e9a1447e5ffcc
kimikimi714 <kimikimi714@gmail.com>
2023-08-07 02:15:16 +0900

    :+1: add test of Verify

--- controller/slack_test.go
+++ controller/slack_test.go
@@ -2,11 +2,91 @@ package controller
 
 import (
 	"encoding/json"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func newHeader(sign, timestamp string) http.Header {
+	h := http.Header{}
+	h.Set("X-Slack-Signature", sign)
+	h.Set("X-Slack-Request-Timestamp", timestamp)
+	return h
+}
+
+func TestVerify(t *testing.T) {
+	tests := []struct {
+		name       string
+		signature  string
+		timestamp  string
+		secret     string
+		errMessage string
+	}{
+		{
+			name:       "headers missing",
+			signature:  "",
+			timestamp:  "",
+			secret:     "",
+			errMessage: "Required headers are missing.",
+		},
+		{
+			name:       "signature header missing",
+			signature:  "",
+			timestamp:  "11111",
+			secret:     "",
+			errMessage: "Required headers are missing.",
+		},
+		{
+			name:       "timestamp header missing",
+			signature:  "aaa",
+			timestamp:  "",
+			secret:     "",
+			errMessage: "Required headers are missing.",
+		},
+		{
+			name:       "secret is empty",
+			signature:  "aaa",
+			timestamp:  "dummy",
+			secret:     "",
+			errMessage: "SLACK_SIGINING_SECRET is empty.",
+		},
+		{
+			name:       "expired timestamp",
+			signature:  "aaa",
+			timestamp:  "111",
+			secret:     "dummy",
+			errMessage: "Expired timestamp.",
+		},
+		{
+			name:       "cannot parse timestamp",
+			signature:  "aaa",
+			timestamp:  "dummy",
+			secret:     "dummy",
+			errMessage: "Cannot parse X-Slack-Request-Timestamp header.",
+		},
+		{
+			name:       "expired timestamp",
+			signature:  "aaa",
+			timestamp:  strconv.FormatInt(time.Unix(time.Now().Unix()+60*3, 0).Unix(), 10),
+			secret:     "dummy",
+			errMessage: "Expired timestamp.",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := SlackController{}
+			err := c.Verify(newHeader(tt.signature, tt.timestamp), []byte{}, tt.secret)
+			if err != nil && !strings.Contains(err.Error(), tt.errMessage) {
+				t.Fatalf("failed test: %+v", err)
+			}
+		})
+	}
+}
 
 func TestParseRequest(t *testing.T) {
 	tests := []struct {
