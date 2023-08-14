bbbfd15104eb272d2af1e7ab217116a4d9acea60
kimikimi714 <kimikimi714@gmail.com>
2023-08-15 00:51:01 +0900

    :+1: いろいろ修正

--- slack.go
+++ slack.go
@@ -2,12 +2,13 @@ package jimiko
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/kimikimi714/jimiko/controller"
+	"github.com/kimikimi714/jimiko/log"
 )
 
 // Slack is Slack向けep
@@ -16,29 +17,42 @@ func Slack(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
+		log.Error("Cannot read request body. err is %s.", err)
 		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 	secret := os.Getenv("SLACK_SIGINING_SECRET")
-	err = c.Verify(r.Header, string(body), secret)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		log.Fatalf("err is: %s", err)
-	}
-
-	var d controller.SlackRequestBody
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Fatalf("failed to parse: %v", r.Body)
-	}
-
-	// 地味子にメンション付きで話しかけないと反応しない
-	if d.Event.Type != "app_mention" {
+	if err := c.Verify(r.Header, string(body), secret); err != nil {
+		log.Error("SlackController.Verify got error: %s.", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	err = c.Reply(d)
-	if err != nil {
+	var d controller.SlackRequestBody
+	if err := json.Unmarshal(body, &d); err != nil {
+		log.Error("Failed to parse request body: %s.", string(body))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if d.Type == "url_verification" {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprint(w, d.Challenge)
+		return
+	}
+
+	// FIXME: slash command 対応
+	// メンションじゃなくてコマンドで聞けるようにしたい
+	// 地味子にメンション付きで話しかけないと反応しない
+	if d.Type != "event_callback" || d.Event.Type != "app_mention" {
+		log.Warn("Not accepted event type:  %s / %s.", d.Type, d.Event.Type)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if err := c.Reply(d); err != nil {
+		log.Error("SlackController.Reply got error: %s.", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
